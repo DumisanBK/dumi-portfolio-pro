dcd725972e61172e3fe89c012f83c2e73996c795
dkaunda <dumisani.kaunda@nbs.mw>
2022-01-27 16:43:01 +0200

    Implemented the smooth scrolling

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,6 +1,130 @@
-import React from "react";
-import imgBack from "../../src/images/mailz.jpeg";
+import React, { useState } from "react";
+import Typical from "react-typical";
+import imgBack from "../../assets/images/mailz.jpeg";
+import load1 from "../../assets/images/load2.gif";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
 
-export default function ContactMe() {
-  return <div></div>;
+import "./ContactMe.css";
+import Footer from "../Home/Footer/Footer";
+
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export default function ContactMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data = {
+        name,
+        email,
+        message,
+      };
+
+      setBool(true);
+
+      const res = await axios.post("/contact", data);
+      if (name.length === 0 || email.length === 0 || message.length === 0) {
+        setBanner(res.data.msg);
+        toast.error(res.data.msg);
+        setBool(false);
+      } else if (res.status === 200) {
+        setBanner(res.data.msg);
+        toast.success(res.data.msg);
+        setBool(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="main-container fade-in" id={props.id || ""}>
+      <ScreenHeading subHeading={"Lets Keep In Touch"} title={"Contact Me"} />
+      <div className="central-form">
+        <div className="col">
+          <h2 className="title">
+            <Typical
+              loop={Infinity}
+              steps={[
+                "Get in touch with me ✉",
+                1000,
+                "Get your job done ✅",
+                1000,
+              ]}
+            />
+          </h2>{" "}
+          <a href="https://www.facebook.com/multicsystech">
+            <i className="fa fa-facebook-square"></i>
+          </a>{" "}
+          <a href="https://www.instagram.com/dumisanbkaunda/">
+            <i className="fa fa-instagram"></i>
+          </a>{" "}
+          <a href="https://www.linkedin.com/in/dumisani-brown-kaunda-427292121/">
+            <i className="fa fa-linkedin-square"></i>
+          </a>{" "}
+          <a href="https://www.multicsystems.com/">
+            <i className="fa fa-globe"></i>
+          </a>
+        </div>
+        <h2 className="big-heading"> Enter your details below</h2>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send Your Email Here!</h4>
+            <img src={imgBack} alt="not found" />
+          </div>
+          <form onSubmit={handleSubmit}>
+            <p>{banner}</p>
+            <label htmlFor="name">Name</label>
+            <input type="text" onChange={handleName} value={name} />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" onChange={handleEmail} value={email} />
+
+            <label htmlFor="message">Message</label>
+            <textarea type="text" onChange={handleMessage} value={message} />
+
+            <div className="send-btn">
+              <button type="submit">
+                Send
+                <i className="fa fa-paper-plane" />
+                {bool ? (
+                  <b className="load">
+                    <img src={load1} alt=" not responding" />
+                  </b>
+                ) : (
+                  ""
+                )}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
--- src/PortfolioContainer/Home/Profile/Profile.js
+++ src/PortfolioContainer/Home/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typical from "react-typical";
 import "./Profile.css";
+import ScrollService from "../../../utilities/ScrollService";
 
 function Profile() {
   return (
@@ -17,6 +18,9 @@ function Profile() {
               </a>{" "}
               <a href="https://www.linkedin.com/in/dumisani-brown-kaunda-427292121/">
                 <i className="fa fa-linkedin-square"></i>
+              </a>
+              <a href="https://www.multicsystems.com/">
+                <i className="fa fa-globe"></i>
               </a>
             </div>
           </div>
@@ -51,7 +55,10 @@ function Profile() {
             </span>
           </div>
           <div className="profile-options">
-            <button className="btn primary-btn">
+            <button
+              className="btn primary-btn"
+              onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            >
               {""}
               Hire Me{" "}
             </button>
